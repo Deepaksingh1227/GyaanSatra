81807b78e0769a82590aeac26b164f598c8e495c
deepaksingh1227 <divyanthakur856@gmail.com>
2025-07-15 10:48:25 +0530

    updated

--- frontend/src/notes/UploadForm.jsx
+++ frontend/src/notes/UploadForm.jsx
@@ -1,6 +1,6 @@
 // src/notes/UploadForm.jsx
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import axios from "../api/axios"; // ✅ custom axios instance
 
 const UploadForm = () => {
   const user = JSON.parse(localStorage.getItem("user"));
