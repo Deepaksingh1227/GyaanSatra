cc536c27df24c34e6938ed58d085ad892cf11804
deepaksingh1227 <divyanthakur856@gmail.com>
2025-08-22 21:28:15 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // ✅ Middleware
 app.use(cors({
-  origin: "https://gyaansatra.netlify.app", // ✅ must match Netlify frontend
+  origin: ["http://localhost:5173", "https://gyaansatra.netlify.app"],
   credentials: true
 }));
 app.use(express.json());
