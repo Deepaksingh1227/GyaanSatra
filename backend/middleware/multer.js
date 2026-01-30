5151bb064fc328217896ed069ec8b206803c3fe1
deepaksingh1227 <divyanthakur856@gmail.com>
2026-01-30 23:24:00 +0530

    done

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -28,7 +28,7 @@ const upload = multer({
   storage,
   fileFilter,
   limits: {
-    fileSize: 10 * 1024 * 1024 // Increased to 10MB
+    fileSize: 50 * 1024 * 1024 // Increased to 50MB
   }
 });
 
